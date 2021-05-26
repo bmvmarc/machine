6e28696e18f7fdd4902fd2ff965dfa3aa887b5b4
Marina <bmvmarc@gmail.com>
2021-05-26 21:55:24 +0200

    get rid of inputs in the class
    The class should not use system input at all; it will only handle the input that comes to it via this method and its string argument.

--- coffee_machine.py
+++ coffee_machine.py
@@ -14,21 +14,28 @@ class CoffeeMachine:
 
         self.available = {'water': water, 'milk': milk, 'coffee': coffee, 'cups': cups}
         self.cash = cash
+        self.status = 'initial'
 
     def state(self):
-        print('The coffee machine has:\n'
+        print('\nThe coffee machine has:\n'
               f'{self.available["water"]} of water\n'
               f'{self.available["milk"]} of milk\n'
               f'{self.available["coffee"]} of coffee beans\n'
               f'{self.available["cups"]} of disposable cups\n'
               f'{self.cash} of money\n')
 
-    def buy(self):
-        kind = input('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu::\n')
+    def buy(self, kind):
+
+        if kind == 'back':
+            self.status = 'initial'
+            return
+
         kind = 'espresso' if kind == '1' else 'latte' if kind == '2' else 'cappuccino' if kind == '3' else ''
 
         if not kind:
             return
+        else:
+            self.status = 'initial'
 
         lack = [k for k, v in self.ingredients[kind].items() if v > self.available[k]]
 
@@ -41,28 +48,67 @@ class CoffeeMachine:
 
             self.cash += self.prices[kind]
 
-    def fill(self):
-        try:
-            self.available['water'] += int(input('Write how many ml of water you want to add:\n'))
-            self.available['milk'] += int(input('Write how many ml of milk you want to add:\n'))
-            self.available['coffee'] += int(input('Write how many grams of coffee beans you want to add:\n'))
-            self.available['cups'] += int(input('Write how many disposable coffee cups you want to add:\n'))
-        except ValueError:
-            print('Failed to add ingredients. Try again')
-
     def take(self):
-        print('I gave you ${self.cash}')
+        print(f'I gave you ${self.cash}\n')
         self.cash = 0
 
-    def start(self):
-        action = ''
-        while action != 'exit':
-            action = input('Write action (buy, fill, take, remaining, exit):\n')
-            if action in ('buy', 'fill', 'take'):
-                getattr(self, action)()
-            elif action == 'remaining':
+    def process(self, entry):
+        if self.status == 'initial':
+            if entry == 'buy':
+                print('What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu:')
+                self.status = 'choose_coffee'
+
+            elif entry == 'fill':
+                print('\nWrite how many ml of water you want to add:')
+                self.status = 'enter_water'
+
+            elif entry == 'take':
+                self.take()
+
+            elif entry == 'remaining':
                 self.state()
+
+        elif self.status == 'choose_coffee':
+            self.buy(entry)
+
+        elif self.status == 'enter_water':
+            try:
+                self.available['water'] += int(entry)
+                print('Write how many ml of milk you want to add:')
+                self.status = 'enter_milk'
+            except ValueError:
+                print('Failed to add the ingredient. Try again')
+
+        elif self.status == 'enter_milk':
+            try:
+                self.available['milk'] += int(entry)
+                print('Write how many grams of coffee beans you want to add:')
+                self.status = 'enter_coffee'
+            except ValueError:
+                print('Failed to add the ingredient. Try again')
+
+        elif self.status == 'enter_coffee':
+            try:
+                self.available['coffee'] += int(entry)
+                print('Write how many disposable coffee cups you want to add:')
+                self.status = 'enter_cups'
+            except ValueError:
+                print('Failed to add the ingredient. Try again')
+
+        elif self.status == 'enter_cups':
+            try:
+                self.available['cups'] += int(entry)
+                self.status = 'initial'
+            except ValueError:
+                print('Failed to add the ingredient. Try again')
+
+        if self.status == 'initial':
+            print('Write action (buy, fill, take, remaining, exit):')
 
 
 coffee_machine = CoffeeMachine(400, 540, 120, 9, 550)
-coffee_machine.start()
+
+ent = ''
+while ent != 'exit':
+    coffee_machine.process(ent)
+    ent = input()
